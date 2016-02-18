c0e14db28a552afce31d33424ab2f4004cf71892
dead_horse <dead_horse@qq.com>
2016-02-18 15:18:57 +0800

    deps: fs-extra@0.26

--- test/index.js
+++ test/index.js
@@ -3,19 +3,19 @@ var fs = require('..');
 var assert = require('assert');
 
 describe('co-fs methods', function () {
-  it('should be exports', function (done) {
-    co(function* () {
+  it('should be exports', function () {
+    return co(function* () {
       var ret = yield fs.exists('test/fixtures/msg.json');
       assert(true === ret);
-    })(done);
+    });
   });
 });
 
 describe('fs-extra methods', function () {
-  it('should be wrapped', function (done) {
-    co(function* () {
+  it('should be wrapped', function () {
+    return co(function* () {
       var data = yield fs.readJson('test/fixtures/msg.json');
       assert('hello' === data.msg);
-    })(done);
+    });
   });
 });
